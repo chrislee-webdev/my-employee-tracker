bdb291369779b6af4f20eef765d952a8a859fc33
Christopher <leechrs1@gmail.com>
2022-05-28 22:32:03 -0500

    add and delete employees

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const { status } = require('express/lib/response');
 
@@ -13,6 +14,36 @@ app.get('/', (req, res) => {
         message: 'Hello World'
     })
 })
+
+//Connect to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user:'',
+    password:'',
+    database: 'employees'
+},
+console.log('Connected to the employees database.')
+)
+
+// db.query(`SELECT * FROM employees`, (err, rows) => {
+//     console.log(rows);
+// });
+
+//Get a single emplyee
+db.query(`SELECT * FROM employees WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+//Delete an employee
+db.querry(`DELETE FROM employee WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(result);
+});
 
 app.use((req, res) => {
     res.status(404).end();
