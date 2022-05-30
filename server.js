74ad86ef599ae35cbaa0b3898018ce49bfd9a483
Christopher <leechrs1@gmail.com>
2022-05-30 10:45:52 -0500

    add initial prompt?

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const express = require('express');
 const { status } = require('express/lib/response');
+const inquirer = require('inquirer');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -25,6 +26,74 @@ const db = mysql.createConnection({
 console.log('Connected to the employees database.')
 )
 
+//Inital prompt
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'mainMenu',
+            message: 'What would you like to do?',
+            choices: ['View all departments', "View all roles", 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role' ]
+        },
+    ]).then((answers) => {
+        const { choices } = answers;
+        if (choices === 'View all departments') {
+            //View departments
+            viewDepatments();
+        }
+        if (choices === 'View all roles') {
+            //View roles
+            viewRoles();
+        }
+        if (choices === 'View all employess') {
+            viewEmployees();
+        }
+        if (choices === 'Add a department') {
+            //Add department
+            addDepartment();
+        }
+        if (choices === 'Add a role') {
+            addRole();
+        }
+        if (choices === 'Add an employee') {
+            addEmployee();
+        }
+        if (choices === 'Update an employee role') {
+            updateEmployeeRole();
+        }
+    }) 
+}
+
+//viewDepartments function
+const viewDepartments = () => {
+    
+}
+//viewRoles function
+//viewEmployees function
+//addDepartment function
+//addRole
+//addEmployee
+//updateEmployeeRole
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // db.query(`SELECT * FROM employees`, (err, rows) => {
 //     console.log(rows);
 // });
@@ -40,12 +109,12 @@ db.query(`SELECT * FROM employees WHERE id = 1`, (err, row) => {
 //Create an employee
 const sql = `INSERT INTO employee (id, first_name, last_name, VALUES (?, ?, ?, ?)`;
 
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
-})
+// db.query(sql, params, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
 
 
 
@@ -56,6 +125,8 @@ db.query(sql, params, (err, result) => {
 //     }
 //     console.log(result);
 // });
+
+
 
 app.use((req, res) => {
     res.status(404).end();
