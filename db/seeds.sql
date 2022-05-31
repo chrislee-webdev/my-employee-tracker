f38d28c8f62cfda274fe1bce4ad7b536ca8aeae0
Christopher <leechrs1@gmail.com>
2022-05-30 20:57:11 -0500

    working on seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,8 @@
 INSERT INTO department
     (department_name)
+
+INSERT INTO roles
+    (id, title, salary, department_id)
+
+INSERT INTO employee
+    (id, first_name, last_name, role_id, manager_id)
