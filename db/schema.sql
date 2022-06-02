4eafe49868cfbc6b65a3fe0f3c86ec7e7fcb032f
Christopher <leechrs1@gmail.com>
2022-06-01 21:43:37 -0500

    added null values in seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,26 +4,20 @@ DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
     id INT PRIMARY KEY,
-    department_name VARCHAR(30)
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
     id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department INT,
-    CONSTRAINT fk_dept FOREIGN KEY (department) REFERENCES department(id)
+    department_id INT
 );
 
 CREATE TABLE employee (
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    salary DECIMAL,
-    role INT,
-    manager_id INT,
-    department INT,
-    CONSTRAINT fk_role FOREIGN KEY (role) REFERENCES role(id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id),
-    CONSTRAINT fk_department FOREIGN KEY (department) REFERENCES department(id)
+    role_id INT,
+    manager_id INT NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO department
-    (id, department_name)
+    (id, name)
     VALUES 
     (1, 'Human Resources'),
     (2, 'Accounting'),
@@ -8,26 +8,26 @@ INSERT INTO department
     (5, 'Sales');
 
 INSERT INTO role
-    (id, title, salary)
+    (id, title, salary, department_id)
     VALUES
-    (1, 'HR Manager', 200),
-    (2,'HR Associate', 150),
-    (3,'Accounting Manager', 200),
-    (4,'Accouting Intern', 100),
-    (5,'Operations Manager', 200),
-    (6,'Operations Associate', 150),
-    (7,'Digital Marketing Manager', 200),
-    (8,'Digital Marketing Associate', 150),
-    (9,'Sales Manager', 200),
-    (10,'Sales Associate', 150);
+    (1, 'HR Manager', 200, 1),
+    (2,'HR Associate', 150, 1),
+    (3,'Accounting Manager', 200, 2),
+    (4,'Accouting Intern', 100, 2),
+    (5,'Operations Manager', 200, 3),
+    (6,'Operations Associate', 150, 3),
+    (7,'Digital Marketing Manager', 200, 4),
+    (8,'Digital Marketing Associate', 150, 4),
+    (9,'Sales Manager', 200, 5),
+    (10,'Sales Associate', 150, 5);
 
 INSERT INTO employee
-    (id, first_name, last_name, salary)
+    (id, first_name, last_name, role_id, manager_id)
     VALUES
-    (1, 'Jessie', 'Reese', 200),
-    (2, 'Clay', 'Snyder', 1501),
-    (3, 'Santos', 'Conner', 200),
-    (4, 'Jeffrey', 'Hudson', 100),
-    (5, 'Kimberly', 'Harrison', 200);
+    (1, 'Jessie', 'Reese', 1, NULL),
+    (2, 'Clay', 'Snyder',  6, 5),
+    (3, 'Santos', 'Conner', 5, NULL),
+    (4, 'Jeffrey', 'Hudson', 2, 1),
+    (5, 'Kimberly', 'Harrison', 9, NULL);
 
 
